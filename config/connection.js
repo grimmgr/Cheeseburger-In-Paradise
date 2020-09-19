79bbd96f6ca6324f4bc223a9c277c8fa79d7a40f
grimmgr <ggrimm33@gmail.com>
2020-09-19 14:18:25 -0700

    set up jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL db.
 const mysql = require('mysql');
+let connection;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burger_db'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burger_db'
+  });
+};
+
+
 
 // Make db.
 db.connect(function(err) {
